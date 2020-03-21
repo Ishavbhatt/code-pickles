10cd3b4cec83a4f6361ad71d2c93e4531d50162d
Ishav bhatt <ishavbhattkhaniyara.com>
2020-03-21 11:58:19 +0530

    add header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 
 const Header = () => (
-    <header>
-        <div className="left-section">
-        </div>
-        <nav>
-            
-        </nav> 
-    </header>
-)
+  <header>
+    <div className="left-section"></div>
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Home <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Features
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Pricing
+            </a>
+          </li>
+        </ul>
+        <span class="navbar-text">Navbar text with an inline element</span>
+      </div>
+    </nav>
+  </header>
+);
 
-export default Header
+export default Header;
